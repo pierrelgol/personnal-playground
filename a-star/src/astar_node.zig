993a19cdf2448d0bb3a0616836649a5a76658173
PierreLgol <plgol.perso@gmail.com>
2024-08-29 12:27:00 +0200

    upt

--- a-star/src/astar_node.zig
+++ a-star/src/astar_node.zig
@@ -1,6 +1,18 @@
+// ************************************************************************** //
+//                                                                            //
+//                                                        :::      ::::::::   //
+//   astar_node.zig                                     :+:      :+:    :+:   //
+//                                                    +:+ +:+         +:+     //
+//   By: pollivie <pollivie.student.42.fr>          +#+  +:+       +#+        //
+//                                                +#+#+#+#+#+   +#+           //
+//   Created: 2024/08/29 12:17:57 by pollivie          #+#    #+#             //
+//   Updated: 2024/08/29 12:17:57 by pollivie         ###   ########.fr       //
+//                                                                            //
+// ************************************************************************** //
+
 const std = @import("std");
 const io = std.io;
-const fmt = std.fmt;
+const formt = std.fmt;
 const mem = std.mem;
 const math = std.math;
 const heap = std.heap;
@@ -48,6 +60,17 @@ pub fn AstarNode(comptime T: type) type {
             else
                 return Order.eq;
         }
+
+        pub fn format(
+            self: @This(),
+            comptime fmt: []const u8,
+            options: std.fmt.FormatOptions,
+            writer: anytype,
+        ) !void {
+            _ = fmt;
+            _ = options;
+            try writer.print("[item : {any}, g_score : {e}, h_score : {e}, f_score : {e}, parent : {?}] ", .{ self.item, self.g_score, self.h_score, self.f_score, self.parent });
+        }
     };
 }
 
@@ -78,12 +101,15 @@ test "compare(self: *const Node, other: *const Node) Order" {
     const inputs = testing.fuzzInput(.{});
     const start = inputs;
     const end = inputs;
+
     var node_a = AstarNode([]const u8).init(start);
     node_a.h_score = fakeScores(node_a.item, end);
     node_a.update();
+
     var node_b = AstarNode([]const u8).init(start);
     node_b.h_score = fakeScores(node_b.item, end);
     node_b.update();
+
     try testing.expectApproxEqRel(fakeScores(start, end), node_a.f_score, 0.0001);
     try testing.expect(node_a.compare(&node_b) == Order.eq);
 }
